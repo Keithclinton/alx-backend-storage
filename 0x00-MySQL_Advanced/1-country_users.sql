b1b433d8cac75aa7c05e961fdf1b122a5271ae06
Keith Clinton <Keithnyaburi@gmail.com>
2024-08-07 16:23:36 +0000

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,15 +1,13 @@
--- Create function SafeDiv that divides (and returns) the first by the second number
--- or returns 0 if second number is equal to 0
--- function SafeDiv takes 2 arguments: a, INT, b, INT
--- returns a / b or 0 if b == 0
+-- Creates a table users with below attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enum of countries: US, CO and TN, never null (default=US)
+-- If table exists, script will not fail, can be executed on any database
 
-DELIMITER //
-
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS FLOAT DETERMINISTIC
-BEGIN
-	RETURN (IF (b = 0, 0, a / b));
-END //
-
-DELIMITER ;
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255),
+       country ENUM ('US', 'CO', 'TN') NOT NULL
+);
